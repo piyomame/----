314cbb6eb66fe6f83f85547f4bf98dd614d29821
shibata keigo <piyomame.7k@gmail.com>
2025-09-21 16:16:46 +0900

    小説を追加

--- pages/kennsakukekka.py
+++ pages/kennsakukekka.py
@@ -21,4 +21,18 @@ if "ハイファンタジー" in st.session_state.option:
 if "学園ダークファンタジー" in st.session_state.option:
     st.header("七つの魔剣が支配する")
     st.subheader("")
-
+if "児童ホラー" in st.session_state.option:
+    st.header("黒ゐ生徒会執行部")
+    st.subheader("")
+if "SF感動ファンタジー" in st.session_state.option:
+    st.header("虹色ほたる")
+    st.subheader("")
+if "タイムスリップファンタジー" in st.session_state.option:
+    st.header("あの日、僕は龍を見た")
+    st.subheader("")
+if "異世界料理ファンタジー" in st.session_state.option:
+    st.header("異世界料理同")
+    st.subheader("")
+if "SFアドベンチャー" in st.session_state.option:
+    st.header("雲の向こう、約束の場所")
+    st.subheader("")
